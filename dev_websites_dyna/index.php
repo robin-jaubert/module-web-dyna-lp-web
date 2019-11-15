565d068da2a7acd49304127b7a39a99768c3eba9
j17006403 <robin.jaubert@etu.univ-amu.fr>
2019-11-15 10:56:01 +0100

    page accueil film 1 finie avec bd

--- dev_websites_dyna/index.php
+++ dev_websites_dyna/index.php
@@ -18,19 +18,15 @@ getBlock('php/film_infos.php', array(
    'title' => 'Split'
 ));
 
-//getBlock('php/ppl_infos.php',array(
-//    'type'=>'director',
-//    'director'=>'M.N.S',
-//    'image'=>'img/director_M.N.S.jpg',
-//    'name'=>'M. Night Shyamalan'
-//));
-//
-//getBlock('php/ppl_infos.php',array(
-//    'type'=>'actor',
-//    'actor'=>['J.McA', 'A.T.J', 'B.B','B.W.H','H.L.R','J.S','N.H','S.A'],
-//    'image'=>['img/actor_james-mcavoy.jpg','img/actor_anya-taylor-joy.jpg','img/actor_betty-buckley.jpg','img/actor_brad-william-henke.jpg','img/actor_haley-lu-richardson.jpg','img/actor_jessica-sula.jpg','img/actor_neal-huff.jpg','img/actor_sebastian-arcelus.jpg',],
-//    'name'=>['James McAvoy','Anya Taylor Joy','Betty Buckley','Brad William Henke','Haley Lu Richardson','Jessica Sula','Neal Huff','Sebastian Arcelus',]
-//));
-//
+getBlock('php/ppl_infos.php',array(
+    'type'=>'director',
+    'movie' => 'Split'
+));
+
+getBlock('php/ppl_infos.php',array(
+    'type'=>'actor',
+    'movie'=>'Split'
+));
+
 //getBlock('php/create_footer.php');
 ?>
--- dev_websites_dyna/php/ppl_infos.php
+++ dev_websites_dyna/php/ppl_infos.php
@@ -1,36 +1,68 @@
 <?php
-if ($data['type'] === 'director') {
-    ?>
-    <section>
-        Réalisateur :
-        <aside class="actorFlex">
-            <figure>
-                <img src=<?= $data['image'] ?> alt=<?= $data['director'] ?>>
-                <figcaption> <?= $data['name'] ?> </figcaption>
-            </figure>
-        </aside>
-    </section>
+include_once 'pdo_helpers.php';
 
-    <?php
-}
-elseif($data['type'] === 'actor'){
-    ?>
-    <section>
-        <aside class="actorFlex">
-            Acteurs:
-            <?php
-            for ($i = 0; $i < 8; $i+=1){
-                ?>
-                <figure>
-                    <img src=<?= $data['image'][$i]?> alt=<?= $data['actor'][$i]?>>
-                    <figcaption> <?= $data['name'][$i] ?> </figcaption>
-                </figure>
 
-                <?php
-            }
-            ?>
-        </aside>
-    </section>
-    <?php
+if (!function_exists('pplmovie')){
+    function pplMovie($data){
+        $role = $data['type'];
+        $movie = $data['movie'];
+        $pdo = pdoCreate('mysql-jaubert.alwaysdata.net', 'jaubert_web_dyna', 'jaubert_web_dyna', 'webdyna');
+        $query = $pdo->prepare('Select picture.path, picture.legend from jaubert_web_dyna.picture
+                                      join jaubert_web_dyna.personHasPicture on picture.id=personHasPicture.idPicture
+                                      join jaubert_web_dyna.person on person.id=personHasPicture.idPerson
+                                      join jaubert_web_dyna.movieHasPerson on person.id=movieHasPerson.idPerson
+                                      join jaubert_web_dyna.movie on  movie.id = movieHasPerson.idMovie
+                                      where jaubert_web_dyna.movie.title =\''. $movie. '\'
+                                      and jaubert_web_dyna.movieHasPerson.role=\''.$role.'\'');
+        $query->execute();
+        if ($query->rowCount() == 0){
+            echo 'Film non trouvé';
+            return null;
+        }
+        else{
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+        }
+    }
 }
+
+
+$papa = pplMovie($data);
 ?>
+
+<?php if ($data['type']==='director'):?>
+    Réalisateur :
+    <aside class="actorFlex">
+        <?php foreach ($papa as $key => $director):?>
+            <?php
+            $path = 'img/'.$director['path'];
+            $legend = $director['legend'];
+
+            ?>
+            <figure>
+                <img src=<?=$path?>>
+                <figcaption>
+                    <?= $legend?>
+                </figcaption>
+            </figure>
+        <?php endforeach; ?>
+    </aside>
+<?php elseif ($data['type']==='actor'):?>
+    Acteurs :
+    <aside class="actorFlex">
+        <?php foreach ($papa as $key => $actor):?>
+            <?php
+            $chemin = 'img/'.$actor['path'];
+            $legende = $actor['legend'];
+            ?>
+            <figure>
+                <img src=<?=$chemin?>>
+                <figcaption>
+                    <?=$legende?>
+                </figcaption>
+            </figure>
+        <?php endforeach;?>
+    </aside>
+<?php else:?>
+    Person not found
+<?php endif;?>
